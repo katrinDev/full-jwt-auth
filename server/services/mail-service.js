22895f04ec930e2ee1b2de496da956c2200377b9
katrinDev <polozkovak8@gmail.com>
2023-06-14 19:04:33 +0300

    add sending email for an account verification

--- server/services/mail-service.js
+++ server/services/mail-service.js
@@ -1,5 +1,31 @@
+const nodemailer = require("nodemailer");
+require("dotenv").config();
+
 class MailService {
-  async sendActivationMail(mail, link) {}
+  constructor() {
+    this.transporter = nodemailer.createTransport({
+      service: process.env.EMAIL_SERVICE,
+      auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS,
+      },
+    });
+  }
+
+  async sendActivationMail(mail, link) {
+    await this.transporter.sendMail({
+      from: process.env.EMAIL_USER,
+      to: mail,
+      subject: "Account activation for " + process.env.API_URL,
+      text: "",
+      html: `
+        <div>
+            <h1>Click the link to activate your account</h1>
+            <a href="${link}">Verification Link</a>
+        </div>
+      `,
+    });
+  }
 }
 
 module.exports = new MailService();
--- server/services/user-service.js
+++ server/services/user-service.js
@@ -7,27 +7,35 @@ const UserDto = require("../dtos/user-dto.js");
 
 class UserService {
   async registration(email, password) {
-    const sameUser = await UserModel.findOne({ email });
-    if (sameUser) {
-      throw new Error(`User with the email ${email} exists already`);
+    try {
+      const sameUser = await UserModel.findOne({ email });
+      if (sameUser) {
+        throw new Error(`User with the email ${email} exists already`);
+      }
+
+      const hashPassword = bcrypt.hashSync(password, 3);
+      const activationLink = uuid.v4(); //returns new random string
+
+      await mailService.sendActivationMail(
+        email,
+        `${process.env.API_URL}/api/activate/${activationLink}`
+      );
+
+      const user = await UserModel.create({
+        email,
+        password: hashPassword,
+        activationLink,
+      });
+
+      const userDto = new UserDto(user);
+      const tokens = tokenService.generateTokens({ ...userDto }); //this function expects a simple objct as a parameter, not instance of UserDto class, so we use spread-operator
+
+      await tokenService.saveToken(userDto.id, tokens.refreshToken);
+
+      return { ...tokens, user: userDto };
+    } catch (e) {
+      throw new Error(e);
     }
-
-    const hashPassword = bcrypt.hashSync(password, 3);
-    const activationLink = uuid.v4(); //returns new random string
-    const user = await UserModel.create({
-      email,
-      password: hashPassword,
-      activationLink,
-    });
-
-    await mailService.sendActivationMail(email, activationLink);
-
-    const userDto = new UserDto(user);
-    const tokens = tokenService.generateTokens({ ...userDto }); //this function expects a simple objct as a parameter, not instance of UserDto class, so we use spread-operator
-
-    await tokenService.saveToken(userDto.id, tokens.refreshToken);
-
-    return { ...tokens, user: userDto };
   }
 }
 
